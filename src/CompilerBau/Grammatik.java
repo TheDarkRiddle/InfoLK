eb033d639e7d6448e9d61e8f4f41fd620c799fb6
TheDarkRiddle <dermoritzlach@gmail.com>
2024-01-23 16:39:24 +0100

    Parser completed

---
+++ src/CompilerBau/Grammatik.java
@@ -0,0 +1,42 @@
+package CompilerBau;
+
+import java.util.HashMap;
+
+/**
+ * Presents a grammatic with all of its productions
+ * */
+public class Grammatik {
+
+    public final HashMap<String, Character> S;
+    public final HashMap<String, Character> A;
+    public final HashMap<String, Character> B;
+    public final HashMap<String, Character> AllProduktions;
+
+
+   public Grammatik()
+   {
+
+       /*
+        * Grammatik:
+        * S-> ZahlA | OperatorB
+        * A-> OperatorB | ZahlB
+        * B->Zahl
+        */
+       S = new HashMap<>();
+       S.put("ZahlA", 'A');
+       S.put("OperatorB",'B');
+
+       A = new HashMap<>();
+       A.put("OperatorB",'B');
+       A.put("ZahlB",'B');
+
+       B = new HashMap<>();
+       B.put("Zahl",' ');
+
+       AllProduktions = new HashMap<>();
+       AllProduktions.put("ZahlA", 'A');
+       AllProduktions.put("OperatorB",'B');
+       AllProduktions.put("ZahlB",'B');
+       AllProduktions.put("Zahl",' ');
+   }
+}
--- src/CompilerBau/Parser.java
+++ src/CompilerBau/Parser.java
@@ -2,33 +2,117 @@ package CompilerBau;
 
 public class Parser {
 
-    public void Parser(){};
+    //Attribute
     //Listen die die Gramatik optionen darstellen
-    private List<String> S = new List<String>();
+    private static Grammatik MGrammatik;
 
 
+    public Parser(Grammatik pG)
+    {
+        MGrammatik = pG;
+    }
+
+    /**
+     * Prüft ob eine Token liste Gültig ist.
+     *
+     * @param L Zu pruefende Token Liste
+     * */
     public boolean Parse(List<String> L)
     {
-        //Check Reihenfolge der Tokens
-        //Liste durchlaufen bis leer
-        //Tokens checken mit IstInG
-        // 0=S 1=A 2=S
-        /*
-        * Grammatik
-        * S-> ZahlA | OperatorB
-        * A-> OperatorB | ZahlB
-        * B->Zahl
-        * */
         L.toFirst();
+        if (!L.hasAccess()) return false;
+        L.toFirst();
+        if (!SindTokenGueltig(L)) return false;
+        L.toFirst();
+        return IstInGrammatik(L, 'S');
+    }
+
+    /**
+     * Prüft ob alle Tokens der Liste auch in der Grammatik vorhanden sind
+     *
+     * @param L Zu pruefende Token Liste
+     * */
+    public boolean SindTokenGueltig(List<String> L)
+    {
         while (L.hasAccess())
         {
-            String tempT = L.getContent();
-        };
-        return false;
-    };
-
-    public int IstInS(String Token)
+            if (!MGrammatik.AllProduktions.containsKey(L.getContent())) return false;
+            L.next();
+        }
+        return true;
+    }
+    /**
+     * Prüft ob die Reihenfolge der Tokens mit der Grammatik konstruierbar ist
+     *
+     * @param TL Eine Liste an Tokens, welche anhand der Grammatik Geprüft werden soll
+     * @param PreviousChar Das Aktuelle Non-Terminal der Grammatik
+     * */
+    public boolean IstInGrammatik(List<String> TL, char PreviousChar)
     {
-        //Prüfen ob das Token wählbar sein
-    };
+        //
+
+        String Token = TL.getContent();
+        Character NextChar;
+        switch (PreviousChar)
+        {
+           case 'S':
+               if (MGrammatik.S.containsKey(Token))
+               {
+                   System.out.print(PreviousChar+"->");
+
+                   NextChar = MGrammatik.S.get(Token);
+                   TL.next();
+                   return IstInGrammatik(TL,NextChar);
+               }else
+               {
+                   System.out.print("Error->");
+                   return false;
+               }
+           case 'A':
+               if (MGrammatik.A.containsKey(Token))
+               {
+                   System.out.print(PreviousChar+"->");
+
+                   NextChar = MGrammatik.A.get(Token);
+                   TL.next();
+                   return IstInGrammatik(TL,NextChar);
+               }else
+               {
+                   System.out.print("Error->");
+                   return false;
+               }
+           case 'B':
+               if (MGrammatik.B.containsKey(Token))
+               {
+                   System.out.print(PreviousChar+"->");
+
+                   NextChar = MGrammatik.B.get(Token);
+                   TL.next();
+                   return IstInGrammatik(TL,NextChar);
+               }else
+               {
+                   System.out.print("Error->");
+                   return false;
+               }
+           case ' ':
+               return !TL.hasAccess();
+            default:
+              return false;
+       }
+    }
+
+    public static void main(String[] args) {
+        Grammatik EineGrammatik = new Grammatik();
+        Parser EinParser = new Parser(EineGrammatik);
+        List<String> EineTokenListe = new List<>();
+
+        EineTokenListe.append("ZahlA");
+        EineTokenListe.append("ZahlB");
+        EineTokenListe.append("Zahl");
+        System.out.println("\n----------------------------------------------");
+        System.out.println("Token Liste Gültig bis: ");
+        System.out.println(" "+EinParser.Parse(EineTokenListe));
+
+
+    }
 }
