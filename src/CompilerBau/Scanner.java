b7fc014e5f0765e3c25212cccd2968beb23e6145
TheDarkRiddle <dermoritzlach@gmail.com>
2024-01-24 18:43:00 +0100

    Scanner works

--- src/CompilerBau/Scanner.java
+++ src/CompilerBau/Scanner.java
@@ -2,53 +2,54 @@ package CompilerBau;
 
 public class Scanner {
     private List<String> TokenListe;
-    public Scanner(String Satzt)
+    public Scanner(String Satz)
     {
         TokenListe = new List<>();
-        SchneiderUndWeber(Satzt);
+        SchneiderUndWeber(Satz);
     }
 
     private String ScannWord(String Wort)
     {
         int Zustand = 0;
-        for (int i = 0; i < Wort.length();i++)
+        for (int i = 0; i <= Wort.length();i++)
         {
             switch (Zustand)
             {
                 case 0:
                     if (Wort.charAt(i) == '1') Zustand = 4;
-                    if (Wort.charAt(i) == '-') Zustand = 1;
-                    if (Wort.charAt(i) == '+') Zustand = 2;
+                    else if (Wort.charAt(i) == '-') Zustand = 1;
+                    else if (Wort.charAt(i) == '+') Zustand = 2;
+                    else return "Noken";
                     break;
                 case 1:
                     if (Wort.charAt(i) == '-') Zustand = 3;
-                    if (Wort.charAt(i) == '1' || Wort.charAt(i) == '0') return "Noken";
+                    else if (Wort.charAt(i) == '1' || Wort.charAt(i) == '0') return "Noken";
                     break;
                 case 2:
                     if (Wort.charAt(i) == '+') Zustand = 3;
-                    if (Wort.charAt(i) == '1' || Wort.charAt(i) == '0') return "Noken";
+                    else return "Noken";
                     break;
                 case 3:
                     if (Wort.charAt(i) == ' ') return "Operator";
-                    if (Wort.charAt(i) == '1' || Wort.charAt(i) == '0') return "Noken";
-                    break;
+                    else return "Noken";
                 case 4:
                     if (Wort.charAt(i) == ' ') return "Zahl";
-                    if (Wort.charAt(i) == '-' || Wort.charAt(i) == '+') return "Noken";
-                    break;
+                    else if (Wort.charAt(i) == '1' || Wort.charAt(i) == '0') break;
+                    else return "Noken";
             }
         }
         return "FatalError";
     }
-    private void SchneiderUndWeber(String Satzt)
+    private void SchneiderUndWeber(String Satz)
     {
-        if (Satzt == null || Satzt.length() == 0) return;
+        if (Satz == null || Satz.isEmpty()) return;
+        if (Satz.charAt(Satz.length()-1) != ' ' ) Satz += " ";
         int WortAnfang = 0;
-        for (int i = 0; i < Satzt.length(); i++)
+        for (int i = 0; i < Satz.length(); i++)
         {
-            if (Satzt.charAt(i) == ' ')
+            if (Satz.charAt(i) == ' ')
             {
-                TokenListe.append(ScannWord(Satzt.substring(WortAnfang, i)));
+                TokenListe.append(ScannWord(Satz.substring(WortAnfang, i)+" "));
                 WortAnfang = i+1;
             }
         }
@@ -58,14 +59,14 @@ public class Scanner {
     public List<String> GetTokenListe(){ return TokenListe;}
 
     public static void main(String[] args) {
-        String EinSatz = "1 + 101001";
+        String EinSatz = "10 ++- 10101";
         Scanner EinScanner = new Scanner(EinSatz);
         List<String> TokenList = EinScanner.GetTokenListe();
         TokenList.toFirst();
-        System.out.println("\n Token Liste:\n");
+        System.out.println("\n Token Liste:");
         while (TokenList.hasAccess())
         {
-            System.out.println(" "+TokenList.getContent());
+            System.out.print(" "+TokenList.getContent());
             TokenList.next();
         }
     }
